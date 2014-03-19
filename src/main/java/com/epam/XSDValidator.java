f2a9c28a0a27ba988a771fb4c751372fa54f25c8
marat-chardymov <marat.chardymov@gmail.com>
2014-03-19 16:18:04 +0300

    XSD schema is added; validation code added

---
+++ src/main/java/com/epam/XSDValidator.java
@@ -0,0 +1,32 @@
+package com.epam;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class XSDValidator {
+    public static void main(String[] args) throws IOException, SAXException {
+        File schemaFile = new File("schema.xsd");
+        Source xmlFile = new StreamSource(new File("catalog.xml"));
+        SchemaFactory schemaFactory = SchemaFactory
+                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Schema schema = schemaFactory.newSchema(schemaFile);
+        Validator validator = schema.newValidator();
+        try {
+            validator.validate(xmlFile);
+            System.out.println(xmlFile.getSystemId() + " is valid");
+        } catch (SAXException e) {
+            System.out.println(xmlFile.getSystemId() + " is NOT valid");
+            System.out.println("Reason: " + e.getLocalizedMessage());
+        }
+    }
+}
