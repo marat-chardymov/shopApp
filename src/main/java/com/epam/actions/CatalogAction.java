62645453a3b5570ba3bcf8d94a4f205df8d92efc
marat-chardymov <marat.chardymov@gmail.com>
2014-04-08 12:16:55 +0300

    categories list implemented using expression language

--- src/main/java/com/epam/actions/CatalogAction.java
+++ src/main/java/com/epam/actions/CatalogAction.java
@@ -1,5 +1,8 @@
 package com.epam.actions;
 
+import java.io.InputStream;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,11 +10,36 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+import com.epam.forms.ProductsForm;
+import com.epam.util.PathsHolder;
 
 public class CatalogAction extends DispatchAction {
+
+	private static final String CATEGORIES = "categories";
+
 	public ActionForward categories(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		return null;
+		SAXBuilder saxBuilder = new SAXBuilder();
+		InputStream catalogIS = CatalogAction.class
+				.getResourceAsStream(PathsHolder.CATALOG);
+		Document document = saxBuilder.build(catalogIS);
+		Element rootNode = document.getRootElement();
+		List list = rootNode.getChildren();
+		for (int i = 0; i < list.size(); i++) {
+
+			Element node = (Element) list.get(i);
+
+			System.out
+					.println(node.getAttributeValue("name"));
+
+		}
+		ProductsForm productsForm = (ProductsForm) form;
+		productsForm.setDocument(document);
+		return mapping.findForward(CATEGORIES);
 	}
 }
---
+++ src/main/webapp/categories.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@taglib uri="http://struts.apache.org/tags-nested" prefix="nested"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+<%@ taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Catalog</title>
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/app.css" />
+</head>
+<body>
+	<div id="listBlock">
+		<ul>
+			<c:forEach items="${productsForm.document.rootElement.children}" var="category">
+				<li>${category.getAttributeValue("name")} (${category.children.size()})</li>
+				
+			</c:forEach>
+		</ul>
+	</div>
+
+</body>
+</html>
